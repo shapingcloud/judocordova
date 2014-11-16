7aec3e16edba62466c1b9028367f52e13d959420
Carlos Oliveira <carlos@shapingcloud.com>
2014-11-16 16:39:22 +0000

    Full integration into iOS SDK

--- src/ios/JudoPaymentPlugin.m
+++ src/ios/JudoPaymentPlugin.m
@@ -1,8 +1,8 @@
 //
 //  JudoPaymentPlugin.m
-//  HelloWorld
 //
-//  Created by James Isbister on 10/10/2013.
+//  Created by Shaping Cloud on 15/11/2014.
+//  originally forked from https://github.com/edzordzinam/judocordova
 //
 //
 
@@ -28,55 +28,95 @@
 
 -(void)paymentAction:(id)sender withCommand:(CDVInvokedUrlCommand*)command {
     AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-    
-    //[JudoAPIManager setToken:@"JiMUukHHkLyX3cax" andSecret:@"65d81925c1ca0a884c6a4e9f2f4186486e7a91c27cf92244223e40cb0170cef1"];
-    
     float paymentAmount = [[command.arguments objectAtIndex:0] floatValue];
     NSString* paymentId = [command.arguments objectAtIndex:1];
-    NSString* paymentToken = [command.arguments objectAtIndex:2];
-    NSString* paymentRef = [command.arguments objectAtIndex:3];
-    NSString* consumerRef = [command.arguments objectAtIndex:4];
+    NSString* paymentRef = [command.arguments objectAtIndex:2];
+    NSString* consumerRef = [command.arguments objectAtIndex:3];
+    NSDictionary *cardInfo;
+    NSString *jsonStr = [command.arguments objectAtIndex:4];
+    if(jsonStr.length>0){
+        //jsonStr = [jsonStr stringByReplacingOccurrencesOfString:@"\\\"" withString:@"\""];
+        //jsonStr = [NSString stringWithFormat:@"[%@]",jsonStr];
+        cardInfo = [NSJSONSerialization JSONObjectWithData:[jsonStr dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
+    }
+    NSString *token = [command.arguments objectAtIndex:5];
+    NSString *secret = [command.arguments objectAtIndex:6];
+    NSString *env = [command.arguments objectAtIndex:7];
+    
+    if(env=='Staging'){
+    [JudoSDKManager setSandboxMode];
+    }
+    
+    [JudoSDKManager setToken:token andSecret:secret];
+    
+    
     
     NSLog(@"Payment amount: [%f]", paymentAmount);
     NSLog(@"Payment ID: [%@]", paymentId);
-    NSLog(@"Payment Token: [%@]", paymentToken);
     NSLog(@"Payment Ref: [%@]", paymentRef);
     NSLog(@"Consumer Ref: [%@]", consumerRef);
     
-        [JudoAPIManager setoAuthToken:paymentToken];
+    if(![cardInfo objectForKey:@"cardToken"]){ //Do we have stored card details? If not request them
+        [JudoSDKManager
+         makeAPaymentWithAmount:paymentAmount
+         toJudoID:paymentId
+         withPayRef:paymentRef
+         withConsumerRef:consumerRef
+         withMetaData: @{@"TestDescription" : @"Test"}
+         andParentViewController:appDelegate.window.rootViewController
+         withSuccess:^(id receipt) {
+             NSLog(@"Success: %@", receipt);
+             
+             NSString* paymentResult = [receipt valueForKey:@"result"];
+             if((paymentResult != nil)&&([paymentResult isEqualToString:@"Success"])){
+                 
+                 CDVPluginResult *pluginResult = [ CDVPluginResult resultWithStatus : CDVCommandStatus_OK messageAsDictionary : receipt ];
+                 [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+             } else {
+                 CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Cancelled"];
+                 [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+             }
+             
+         } withFailure:^(NSError *error) {
+             NSLog(@"Fail: %@", error);
+             
+             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"user_cancelled"];
+             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+         }];
+    }
+    else{ //Pay with saved card detials
+        [JudoSDKManager
+         makeATokenPaymentWithAmount:paymentAmount
+         withCardDetails:cardInfo
+         toJudoID:paymentId
+         withPayRef:paymentRef
+         withConsumerRef:consumerRef
+         withMetaData: @{@"TestDescription" : @"Test"}
+         andParentViewController:appDelegate.window.rootViewController
+         withSuccess:^(id receipt) {
+             NSLog(@"Success: %@", receipt);
+             
+             NSString* paymentResult = [receipt valueForKey:@"result"];
+             if((paymentResult != nil)&&([paymentResult isEqualToString:@"Success"])){
+                 
+                 CDVPluginResult *pluginResult = [ CDVPluginResult resultWithStatus : CDVCommandStatus_OK messageAsDictionary : receipt ];
+                 [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+             } else {
+                 CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Cancelled"];
+                 [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+             }
+             
+         } withFailure:^(NSError *error) {
+             NSLog(@"Fail: %@", error);
+             
+             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"user_cancelled"];
+             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+         }];
+        
+    }
     
-        [JudoAPIManager simpleMakePaymentWithNavBarAmount:paymentAmount
-                                       toJudoID:paymentId
-                                     withPayRef:paymentRef
-                                withConsumerRef:consumerRef
-                        andParentViewController:appDelegate.window.rootViewController
-                                    withSuccess:^(id JSON) {
-                                                  NSLog(@"Success: %@", JSON);
-                                                  
-                                                  //self.cardInfo = [JSON valueForKey:@"cardDetails"];
-                                                  //self.consumerRef = [[JSON valueForKey:@"consumer"] valueForKey:@"yourConsumerReference"];
-                                                  NSString* paymentResult = [JSON valueForKey:@"result"];
-                                        if((paymentResult != nil)&&([paymentResult isEqualToString:@"Success"])){
-                                        
-                                        
-                                                  CDVPluginResult *pluginResult = [ CDVPluginResult resultWithStatus : CDVCommandStatus_OK messageAsDictionary : JSON ];
-                                                  
-                                                  UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Payment succesful" message:@"Thank you for your purchase" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil];
-                                                  [alertView show];
-                                                  
-                                                  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                                        } else {
-                                            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Cancelled"];
-                                            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                                        }
-                                              }
-                                    withFailure:^(NSError *error) {
-                                                  NSLog(@"Fail: %@", error);
-                                                  
-                                                  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"user_cancelled"];
-                                                  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                                              }
-         ];
+    
+    
 }
 
 @end
